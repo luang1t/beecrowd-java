a9961ebbc3800cd63f140e6eab4b5d911fd44526
luang1t <luanzragit@gmail.com>
2025-06-30 13:15:34 -0300

    Primeiro desafio

---
+++ src/bee1000.java
@@ -0,0 +1,10 @@
+public class bee1000 {
+    // Your first program in any programming language is usually "Hello World!". In
+    // this first problem all you have to do is print this message on the screen.
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
